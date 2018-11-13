3a57227e6772a432b30cc419055db242d9d2a472
Fernando Ojeda <fernando.ojeda@jalasoft.com>
2018-11-13 12:12:50 -0400

    Add homework1 to lesson1

--- fernando-ojeda/lesson-1/homework-1-contacts/ContactsManager.java
+++ fernando-ojeda/lesson-1/homework-1-contacts/ContactsManager.java
@@ -7,12 +7,12 @@ public class ContactsManager {
         myFriends = new Contact[500];
     }
 
-    void addContact(Contact contact){
+    public void addContact(Contact contact){
         myFriends[friendsCount] = contact;
         friendsCount++;
     }
 
-    Contact searchContact(String searchName){
+    public Contact searchContact(String searchName){
         for(int i=0; i<friendsCount; i++){
             if(myFriends[i].name.equals(searchName)){
                 return myFriends[i];
