2134f046e0cbb78af8d688334686d93c5d69dcc9
dxni <33109901+dxni@users.noreply.github.com>
2018-11-13 22:03:12 -0400

    Cars App

---
+++ daniela-ticona/lesson-1/Cars app/src/com/company/Cars.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Cars {
+    String  nameplayer;
+    double performace;
+    double gasTank;
+    double distanceGas;
+
+    Cars(String nameplayer, Double performace, Double gasTank) {
+        this.nameplayer = nameplayer;
+        this.performace = performace;
+        this.gasTank=gasTank;
+        this.distanceGas=distanceGas;
+    }
+
+
+
+}
