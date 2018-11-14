1f7bc17e4296eb0415cc91b6de0d7e6297c75c05
edgartron32@gmail.com <edgartron32@gmail.com>
2018-11-13 23:47:01 -0400

    contact

--- edgar-quispe/task2-contactman/src/ContactsManager.java
+++ edgar-quispe/task2-contactman/src/ContactsManager.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+class ContactsManager {
+    // Fields:
+    Contact [] myFriends;
+    int friendsCount;
 
-public class ContactsManager {
+    // Constructor:
+    ContactsManager(){
+        friendsCount = 0;
+        myFriends = new Contact[500];
+    }
+
+    // Methods:
+    void addContact(Contact contact){
+        myFriends[friendsCount] = contact;
+        friendsCount++;
+    }
+
+    Contact searchContact(String searchName){
+        for(int i=0; i<friendsCount; i++){
+            if(myFriends[i].name.equals(searchName)){
+                return myFriends[i];
+            }
+        }
+        return null;
+    }
 }
