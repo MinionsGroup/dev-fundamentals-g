40ce7c70df13b3de64c6fefebcd5297620b4177c
José Christian Galarza Crespo <yuseisatelite@gmail.com>
2018-11-13 22:32:53 -0400

    Add the second Task
    Homework lesson1 task 2

---
+++ christian-galarza/lesson-1/car_app/src/main/Car.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * This class declare the variables.
+ *
+ *  @author Jose Christian Galarza Crespo
+ *  @version 1.0.
+ */
+class Car {
+    boolean statusEngine = false;
+    double gas = 0;
+    double tankCapacity = 100;
+    double distanceMoved = 0;
+    double performance= 0;
+}
---
+++ christian-galarza/lesson-1/car_app/src/main/Main.java
@@ -0,0 +1,43 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        //create the car with tank capacity and performance.
+        CarManager chariot = new CarManager(0,0);
+        CarManager f1mercedes = new CarManager(100, 0.1);
+        CarManager f1ferrari = new CarManager(100, 0.15);
+        CarManager f1mclaren = new CarManager(105, 0.23);
+        CarManager nissanterrano = new CarManager(300,0.9);
+        //insert the quality of gas and power on the car.
+        f1mercedes.fillGas(100);
+        f1ferrari.fillGas(100);
+        f1mclaren.fillGas(100);
+        nissanterrano.fillGas(300);
+        chariot.powerOff();
+        f1mercedes.powerOn();
+        f1ferrari.powerOn();
+        f1mclaren.powerOn();
+        nissanterrano.powerOn();
+
+        //Print results.
+        System.out.println("-- chariot --");
+        chariot.moveDistance(2);
+        chariot.printInformation();
+
+        System.out.println("\n -- f1 mercedes --");
+        f1mercedes.moveDistance(350);
+        f1mercedes.printInformation();
+
+        System.out.println("\n -- f1 ferrari --");
+        f1ferrari.moveDistance(350);
+        f1ferrari.printInformation();
+
+        System.out.println("\n -- f1 mclaren --");
+        f1mclaren.moveDistance(350);
+        f1mclaren.printInformation();
+
+        System.out.println("\n -- Nissan Terrano --");
+        nissanterrano.moveDistance(300);
+        nissanterrano.printInformation();
+    }
+}
