84333d7c267fc16f3c8284150e09705352b88ccf
dxni <33109901+dxni@users.noreply.github.com>
2018-11-19 18:45:20 -0400

    homework 1 lesson 2

---
+++ daniela-ticona/lesson-2/homework-1-Read-Parameters-Car-app/Cars app/src/com/company/Cars.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Cars {
+    String  nameplayer;
+    double performace;
+    double gasTank;
+    double distance;
+
+    Cars(String nameplayer, Double performace, Double gasTank, Double distance) {
+        this.nameplayer = nameplayer;
+        this.performace = performace;
+        this.distance= distance;
+        this.gasTank=gasTank;
+        //this.distanceGas=distanceGas;
+    }
+
+
+
+}
