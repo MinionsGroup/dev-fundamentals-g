41ad23b3581a1dd6cabbac42102868adc098fb97
yacha <yesica.acha@jalasoft.com>
2018-11-19 21:04:47 -0400

    Task 04 done Load Gas and Mileage from File.txt load performance from Console.

--- marco-mendez/CarProyect/src/Car.java
+++ marco-mendez/CarProyect/src/Car.java
@@ -1,13 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Car {
     private double gas;
     private double mileage;
     private boolean powerOn;
     private double performance;
 
-    public Car(final double gas, final double mileage , final double performance) {
+    public Car(final double gas, final double mileage, final double performance) {
         this.gas = gas;
         this.mileage = mileage;
         this.powerOn = powerOn;
+        this.performance = mileage / performance;
+    }
+
+    public Car(String carFile) throws FileNotFoundException {
+        loadGasAndMileageFromFile(carFile);
+        this.powerOn = false;
+        this.performance = mileage / performance;
+    }
+
+    public void setPerformance(double performance) {
         this.performance = mileage / performance;
     }
 
@@ -36,13 +50,26 @@ public class Car {
         System.out.print("o^o");
     }
 
+    private void loadGasAndMileageFromFile(String carFile) throws FileNotFoundException {
+        File file = new File(carFile);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.contains("Gas")) {
+                this.gas = Double.valueOf(line.split("=")[1]);
+            }
+            if (line.contains("Mileage")) {
+                this.mileage = Double.valueOf(line.split("=")[1]);
+            }
+        }
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         System.out.println("*********Instructions:*********");
         System.out.println(" Each '_' is 0.5 mileage.");
         System.out.println(" 1   2   3   4   5   6   7   8   9  10  11   12   13   14   15   16   17   18   19   20.....KMs");
-        Car car01 = new Car(10, 2.5, 1.5);
+        Car car01 = new Car(10, 1.5, 0.3);
         car01.powerOn();
         car01.move(10);
 
@@ -53,7 +80,38 @@ public class Car {
         Car car03 = new Car(10, 1, 1);
         car03.powerOn();
         car03.move(10);
+
+
+        System.out.println();
+        System.out.println("************************************************************************************");
+        System.out.println("****************** Task - Instance Gas and Mileage from file ***********************");
+
+        /** Instance Cars.  **/
+        Car car04 = new Car("D://CarInformation01.txt");
+        Car car05 = new Car("D://CarInformation02.txt");
+        Car car06 = new Car("D://CarInformation03.txt");
+
+        /** Set Car`s performances.  **/
+        car04.setPerformance(putPerformanceFromConsole("Car 04"));
+        car05.setPerformance(putPerformanceFromConsole("Car 05"));
+        car06.setPerformance(putPerformanceFromConsole("Car 06"));
+
+        /** PowerOn the Cars. **/
+        car04.powerOn();
+        car05.powerOn();
+        car06.powerOn();
+
+        /** Set Distance to Move the cats. **/
+        car04.move(10);
+        car05.move(10);
+        car06.move(10);
+
     }
 
-
+    public static double putPerformanceFromConsole(String carName) {
+        System.out.println(String.format("Put the Performance to: %s ", carName));
+        Scanner scanner = new Scanner(System.in);
+        String performance = scanner.nextLine();
+        return Double.valueOf(performance);
+    }
 }
