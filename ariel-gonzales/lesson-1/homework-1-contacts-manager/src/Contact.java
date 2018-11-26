394b0cf23b2bfe337fea0d6a7529eeffd3686c45
Ariel Gonzales <ariel.gonzales@jalasoft.com>
2018-11-26 17:12:13 -0400

    Tasks Lesson 1

---
+++ ariel-gonzales/lesson-1/homework-1-contacts-manager/src/Contact.java
@@ -0,0 +1,8 @@
+
+/**
+ * contatcs_manager.Contact class.
+ */
+public class Contact {
+    String name;
+    String phoneNumber;
+}
---
+++ ariel-gonzales/lesson-1/homework-2-car-manager/src/Car.java
@@ -0,0 +1,40 @@
+public class Car {
+    private double gasCapacity;
+    private double gasConsume;
+    private double gas;
+    private double mileage;
+    private boolean isPowerOn;
+
+    public Car(double gasCapacity, double gasConsume) {
+        this.gasCapacity = gasCapacity;
+        this.gasConsume = gasConsume;
+        this.gas = 0.0;
+        this.mileage = 0.0;
+        this.isPowerOn = false;
+    }
+
+    public void powerOn() {
+        this.isPowerOn = true;
+    }
+
+    public void powerOff() {
+        this.isPowerOn = false;
+    }
+
+    public void loadGas(double gas) {
+        this.gas = (gas + this.gas > this.gasCapacity) ? this.gasCapacity : gas + this.gas;
+    }
+
+    public void move(double distance) {
+        this.mileage = this.gasConsume * this.gas;
+        this.gas -= (this.gas < (distance / this.gasConsume)) ? this.gas : (distance / this.gasConsume);
+    }
+
+    public double getGas() {
+        return gas;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+}
