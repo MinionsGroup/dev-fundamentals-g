743c34c1b6aa005fc23cb225ed32c18d03d77257
Yury Ortuño Calvo <=>
2018-11-26 19:33:56 -0400

    add task

---
+++ yury-ortuno/lesson-1/homework-1-contacs-manager/src/Contact.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Yury Ortuno on 11/13/2018.
+ */
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
